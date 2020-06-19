04706b938bcb9a1d1b24648b5fd2de410ebc8117
Karthik Kranthi <karthikkranthi@Karthiks-MacBook-Pro.local>
2020-06-20 00:19:27 +0530

    Final App.

---
+++ screens/GameScreen.js
@@ -0,0 +1,155 @@
+import React, { useState, useRef, useEffect } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  Button,
+  Alert,
+  ScrollView,
+  FlatList,
+} from "react-native";
+import { Ionicons } from "@expo/vector-icons";
+
+import NumberContainer from "../components/NumberContainer";
+import Card from "../components/Card";
+import DefaultStyle from "../constants/default-styles";
+import MainButton from "../components/MainButton";
+import Bodytext from "../components/BodyText";
+
+const generateRandomBetween = (min, max, exclude) => {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  const rndNum = Math.floor(Math.random() * (max - min)) + min;
+  if (rndNum === exclude) {
+    return generateRandomBetween(min, max, exclude);
+  } else {
+    return rndNum;
+  }
+};
+
+//ScrollView
+/*const renderListItem = (value, numOfRound) => (
+  <View key={value} style={styles.listItem}>
+    <Bodytext>#{numOfRound}</Bodytext>
+    <Bodytext>{value}</Bodytext>
+  </View>
+);*/
+
+const renderListItem = (listLength, itemData) => (
+  <View style={styles.listItem}>
+    <Bodytext>#{listLength - itemData.index}</Bodytext>
+    <Bodytext>{itemData.item}</Bodytext>
+  </View>
+);
+
+const GameScreen = (props) => {
+  const initialGuess = generateRandomBetween(1, 100, props.userChoice);
+  const [currentGuess, setCurrentGuess] = useState(initialGuess);
+  // const [pastGuesses, setPastGussess] = useState([initialGuess]); // ScrollView
+  const [pastGuesses, setPastGussess] = useState([initialGuess.toString()]);
+  const currentLow = useRef(1);
+  const currentHigh = useRef(100);
+
+  const { userChoice, onGameOver } = props;
+
+  useEffect(() => {
+    if (currentGuess === userChoice) {
+      onGameOver(pastGuesses.length);
+    }
+  }, [currentGuess, userChoice, onGameOver]);
+
+  const nextGuessHandler = (direction) => {
+    if (
+      (direction === "lower" && currentGuess < props.userChoice) ||
+      (direction === "greater" && currentGuess > props.userChoice)
+    ) {
+      Alert.alert("Don't lie", "You know that this is Wrong...", [
+        { text: "Sorry!", style: "cancel" },
+      ]);
+      return;
+    }
+    if (direction === "lower") {
+      currentHigh.current = currentGuess;
+    } else {
+      currentLow.current = currentGuess + 1;
+    }
+    const nextNumber = generateRandomBetween(
+      currentLow.current,
+      currentHigh.current,
+      currentGuess
+    );
+    setCurrentGuess(nextNumber);
+    //  setRounds((curRounds) => curRounds + 1);
+    //  setPastGussess((curPastGuesses) => [nextNumber, ...curPastGuesses]); // ScrollView
+    setPastGussess((curPastGuesses) => [
+      nextNumber.toString(),
+      ...curPastGuesses,
+    ]);
+  };
+
+  return (
+    <View style={styles.screen}>
+      <Text style={DefaultStyle.title}>Opponent's Guess</Text>
+      <NumberContainer>{currentGuess}</NumberContainer>
+      <Card style={styles.buttonContainer}>
+        <MainButton onPress={nextGuessHandler.bind(this, "lower")}>
+          <Ionicons name="md-remove" size={24} color="white" />
+        </MainButton>
+        <MainButton onPress={nextGuessHandler.bind(this, "greater")}>
+          <Ionicons name="md-add" size={24} color="white" />
+        </MainButton>
+      </Card>
+      <View style={styles.listContainer}>
+        {/*   <ScrollView contentContainerStyle={styles.list}>
+          {pastGuesses.map((guess, index) =>
+            renderListItem(guess, pastGuesses.length - index)
+          )}
+        </ScrollView>*/}
+        <FlatList
+          keyExtractor={(item) => item}
+          data={pastGuesses}
+          renderItem={renderListItem.bind(this, pastGuesses.length)}
+          contentContainerStyle={styles.list}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  screen: {
+    flex: 1,
+    padding: 10,
+    alignItems: "center",
+  },
+  buttonContainer: {
+    flexDirection: "row",
+    justifyContent: "space-around",
+    marginTop: 20,
+    width: 400,
+    maxWidth: "90%",
+  },
+  listContainer: {
+    flex: 1,
+    //width: "80%",//ScrollView
+    width: "60%",
+  },
+  list: {
+    flexGrow: 1,
+    //alignItems: "center",//ScrollView
+    justifyContent: "flex-end",
+  },
+  listItem: {
+    borderColor: "#ccc",
+    borderWidth: 1,
+    padding: 15,
+    marginVertical: 10,
+    backgroundColor: "white",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    // width: "60%",// sScrollView
+    width: "100%",
+  },
+});
+
+export default GameScreen;
